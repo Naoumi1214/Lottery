294600be450119989ef38a34684f94570d86ed16
Naoumi1214 <wada121400@gmail.com>
2019-11-19 13:35:08 +0900

    大会詳細画面を実装した

---
+++ database/factories/WinningTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\WinningType;
+use Faker\Generator as Faker;
+
+$factory->define(WinningType::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -4,18 +4,28 @@
 <div class="container">
     <div class="row">
         <div class="competitiondetails col-8">
-            <h1 id="title">{{$name}}</h1>
-            <div class="row">
+            <h1 id="title">TITLE:{{$name}}</h1>
+            <div class="row mx-auto">
                 <img id="icon" src="{{$icon}}"
                     class="col-md-3 img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
                     alt="">
                 <div id="col-md-10 overview">
+                    <h2>概要</h2>
                     <p>{{$overview}}</p>
                 </div>
             </div>
-            <div class="row" 　style="margin-top: 10px;">
-                <p class="ml-sm-3 btn-block"><a href="#" class="btn btn-primary active" role="button">当選番号一覧</a></p>
-                <p class="ml-sm-3 btn-block"><a href="#" class="btn btn-primary active" role="button">当選番号一覧</a></p>
+            <div class="row mx-auto" 　style="margin-top: 10px;">
+                <!--mobile-->
+                <a href="#" class="btn btn-primary active btn-block d-md-none center-block" role="button">当選番号一覧</a>
+                <a href="#" class="btn btn-primary active btn-block d-md-none center-block" role="button">当選を確認</a>
+                <!--PC-->
+                <a href="#" class="btn btn-primary active btn-block d-none d-md-inline col-md-5 ml-md-3"
+                    role="button">当選番号一覧</a>
+                <a href="#" class="btn btn-primary active btn-block d-none d-md-inline col-md-5 ml-md-3"
+                    role="button">当選を確認</a>
+                @if (Auth::check() && Auth::id()===$user_id)
+                <a href="#" class="btn btn-primary active btn-block center-block" role="button">当選を確認</a>
+                @endif
             </div>
         </div>
     </div>
